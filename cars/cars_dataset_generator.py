377ea7d4baa18aa4b46cf106d92a402c34f2b5fe
Juli <july1995bolt@gmail.com>
2018-03-18 20:22:44 -0300

    Create cars_dataset_generator file

---
+++ cars/cars_dataset_generator.py
@@ -0,0 +1,11 @@
+root_folder = 'DataSetAutos/'
+folder_pos_to = 'DataSetAutos/Resize/pos/'
+folder_neg_to = 'DataSetAutos/Resize/neg/'
+final_size = [50, 50]
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
