c37c08de25d372df2621063321d4cc9a83b5a603
genarocamele <genarocamele@hotmail.com>
2018-08-10 17:18:00 -0300

    Para el Robert

--- pedestrian/SVM/svm_clean_detection.py
+++ pedestrian/SVM/svm_clean_detection.py
@@ -1,10 +1,12 @@
 import utils
 import cv2
 import time
+import numpy as np
 
 
-VISUALIZE_SLIDDING_WINDOW = True  # Si esta en True se muestra la ventana deslizante en tiempo real
+VISUALIZE_SLIDDING_WINDOW = False  # Si esta en True se muestra la ventana deslizante en tiempo real
 
+EPSILON = 1.5
 
 def main():
     """La mayoria del codigo se saco de
@@ -13,17 +15,24 @@ def main():
     (win_w, win_h) = (200, 400)
 
     # Cargo y preparo la imagen
-    image = utils.load_image_from_path('/home/genaro/PycharmProjects/proyecto2018/pedestrian/SVM/imgs/street4.jpg')
+    # image = utils.load_image_from_path('/home/genaro/PycharmProjects/proyecto2018/pedestrian/SVM/imgs/street4.jpg')
+    image = cv2.imread('/home/genaro/PycharmProjects/proyecto2018/pedestrian/SVM/imgs/street4.jpg')
+    cv2.imshow("Window final", image)
+    # cv2.waitKey(1)
+    # time.sleep(5.025)
     image = utils.to_grayscale(image)
     image = utils.normalize_image_max(image)
+
+    final_bounding_boxes = np.array([], ndmin=2)
 
     # Obtengo mi SVM
     svm = utils.load_checkpoint('/home/genaro/PycharmProjects/checkpoints_proyecto2018/svmCheckpoint.pkl')
 
     # Loop sobre las diferentes escalas
-    for resized_image in utils.get_pyramid(image, scale=1.5):
+    for i, resized_image in enumerate(utils.get_pyramid(image, scale=EPSILON)):
+        coefficient = EPSILON ** i
         clone = resized_image.copy()  # Hago una copia de la imagen original para graficar detecciones
-        clone_suppression = clone.copy()  # Hago una copia para graficar el resultado de NMS
+        # clone_suppression = clone.copy()  # Hago una copia para graficar el resultado de NMS
 
         # Lista de bounding boxes para el Non Maximal Suppression
         bounding_boxes = []
@@ -40,8 +49,9 @@ def main():
 
             # Inteligencia ACA!
             # Manejo la ventana deslizante actual
-            cropped_image = utils.crop_image(image, x, y, win_w, win_h)  # Recorto la ventana
-            cropped_image = utils.resize(cropped_image, [96, 48])  # Escalo
+            # cropped_image = utils.crop_image(image, x, y, win_w, win_h)  # Recorto la ventana
+            # cropped_image = window
+            cropped_image = utils.resize(window, [96, 48])  # Escalo
             cropped_image_hog = utils.get_hog_from_image(cropped_image, normalize=False)  # Obtengo el HOG
 
             # Comienzo a predecir
@@ -49,36 +59,51 @@ def main():
             if prediction == 1:
 
                 # Si es un peaton guardo el bounding box
-                bounding_box = (x, y, x + win_w, y + win_h)
+                bounding_box = (
+                    x * coefficient,
+                    y * coefficient,
+                    (x + win_w) * coefficient,
+                    (y + win_h) * coefficient
+                )
                 bounding_boxes.append(bounding_box)
                 count_bounding_boxes += 1
 
                 # Si es un peaton grafico la ventana deslizante
-                if VISUALIZE_SLIDDING_WINDOW:
-                    cv2.rectangle(clone, (x, y), (x + win_w, y + win_h), (0, 255, 0), 2)  # Rectangulo verde
+                # if VISUALIZE_SLIDDING_WINDOW:
+                #     cv2.rectangle(clone, (x, y), (x + win_w, y + win_h), (0, 255, 0), 2)  # Rectangulo verde
 
             # Si se quiere ver la ventana deslizante la grafico y la muestro
-            if VISUALIZE_SLIDDING_WINDOW:
-                cv2.rectangle(clone, (x, y), (x + win_w, y + win_h), (0, 0, 0), 2)  # Rectangulo negro
-                cv2.imshow("Slidding Window", clone)
-                cv2.waitKey(1)
-                time.sleep(0.025)
+            # if VISUALIZE_SLIDDING_WINDOW:
+            #     cv2.rectangle(clone, (x, y), (x + win_w, y + win_h), (0, 0, 0), 2)  # Rectangulo negro
+            #     cv2.imshow("Slidding Window", clone)
+            #     cv2.waitKey(1)
+            #     time.sleep(0.025)
 
-        bounding_boxes_suppressed = utils.non_max_suppression_fast(bounding_boxes, 0.3)
+        final_bounding_boxes = np.concatenate((final_bounding_boxes, utils.non_max_suppression_fast(bounding_boxes, 0.3)))
 
         print("Cantidad de bounding boxes antes de NMS --> {}".format(len(bounding_boxes)))
 
         # Loop sobre las ventanas deslizantes suprimidas
-        for (startX, startY, endX, endY) in bounding_boxes_suppressed:
-            cv2.rectangle(clone_suppression, (startX, startY), (endX, endY), (0, 255, 0), 2)  # Rectangulo verde
+        # for (startX, startY, endX, endY) in bounding_boxes_suppressed:
+        #     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)  # Rectangulo verde
 
-        print("Cantidad de bounding boxes despues de NMS --> {}".format(len(bounding_boxes_suppressed)))
+
+
 
         # Si hay bounding boxes productos del NMS graficados, muestro la imagen
-        if len(bounding_boxes_suppressed):
-            cv2.imshow("Window final", clone_suppression)
-            cv2.waitKey(1)
-            time.sleep(5.025)
+        # if len(bounding_boxes_suppressed):
+        #     cv2.imshow("Window final", image)
+        #     cv2.waitKey(1)
+        #     time.sleep(5.025)
+    print(final_bounding_boxes)
+    print(final_bounding_boxes.shape)
+    for (startX, startY, endX, endY) in final_bounding_boxes:
+        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)  # Rectangulo verde
+    print("Cantidad de bounding boxes despues de NMS --> {}".format(len(final_bounding_boxes)))
+
+    cv2.imshow("Window final", image)
+    cv2.waitKey(1)
+    time.sleep(5.025)
 
 
 if __name__ == '__main__':
